31ee5158bd535246982e3103f5aba8594a9a87ca
yh9035926 <yh9035926@naver.com>
2024-04-25 10:38:01 +0900

    slider

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,6 +1,8 @@
 import { useNavigate, useParams } from "react-router-dom";
 import projectsData from "../data/projects.json";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 interface ProjectPageProps {
   setIsMainPage: React.Dispatch<React.SetStateAction<boolean>>;
 }
@@ -20,17 +22,32 @@ const ProjectPage: React.FC<ProjectPageProps> = ({ setIsMainPage }) => {
     setIsMainPage(true);
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <div className="projectPageWrapper">
       <h2 className="projectName">{project.name}</h2>
-      {project.imgs.map((img, index) => (
-        <img
-          key={index}
-          src={require(`../img/${img}`)}
-          alt={`${project.name} 이미지 ${index}`}
-        />
-      ))}
-      <p>{project.description}</p>
+      <div className="projectItem">
+        <Slider {...settings}>
+          {project.imgs.map((img, index) => (
+            <img
+              className="projectImg"
+              key={index}
+              src={require(`../img/${img}`)}
+              alt={`${project.name} 이미지 ${index}`}
+            />
+          ))}
+        </Slider>
+        <p>{project.description}</p>
+      </div>
       <button onClick={handleBack} className="backButton">
         뒤로
       </button>
