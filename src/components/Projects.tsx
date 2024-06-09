0430fdb469e31238badb2788278f1a50816d5af2
yh9035926 <yh9035926@naver.com>
2024-06-09 11:02:17 +0900

    spring carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,79 @@
 import DownButton from "./DownButton";
 import { Link } from "react-router-dom";
-import projectsData from "../data/projects.json"; // Assuming the JSON file is in the same directory
+import projectsData from "../data/projects.json";
+import { useState } from "react";
+import { useSpring, animated, config } from "react-spring";
+import Carousel from "react-spring-3d-carousel";
 
 interface ProjectsProps {
   id: string;
   downButton: () => void;
 }
 
+const Card = ({
+  id,
+  imagen,
+  name,
+}: {
+  id: number;
+  imagen: any;
+  name: string;
+}) => {
+  const [show, setShown] = useState(false);
+
+  const props3 = useSpring({
+    opacity: 1,
+    transform: show ? "scale(1.03)" : "scale(1)",
+    boxShadow: show
+      ? "0 20px 25px rgb(0 0 0 / 25%)"
+      : "0 2px 10px rgb(0 0 0 / 8%)",
+  });
+  return (
+    <animated.div
+      className="card"
+      style={props3}
+      onMouseEnter={() => setShown(true)}
+      onMouseLeave={() => setShown(false)}
+    >
+      <img src={imagen} alt="" />
+      <Link to={`/projects/${id}`} className="goToProject">
+        {name}
+      </Link>
+    </animated.div>
+  );
+};
+
 const Projects = ({ id, downButton }: ProjectsProps) => {
+  const [goToSlide, setGoToSlide] = useState<number | undefined>(undefined);
+
+  const projects = projectsData.map((project) => ({
+    key: project.id,
+    content: (
+      <Card
+        id={project.id}
+        imagen={require(`../img/${project.mainImg[0]}`)}
+        name={project.name}
+      />
+    ),
+  }));
+
+  const table = projects.map((element, index) => {
+    return { ...element, onClick: () => setGoToSlide(index) };
+  });
+
+  const [cards] = useState(table);
+
   return (
     <section className="projectsWrapper" id={id}>
       <h2>Projects</h2>
       <div className="projects">
-        {projectsData.map((project) => (
-          <div key={project.id}>
-            <Link to={`/projects/${project.id}`}>
-              <div className="project">
-                <img
-                  src={require(`../img/${project.mainImg}`)}
-                  alt={project.name}
-                />
-              </div>
-              <div className="projectName">{project.name}</div>
-            </Link>
-          </div>
-        ))}
+        <Carousel
+          slides={cards}
+          goToSlide={goToSlide}
+          offsetRadius={1}
+          showNavigation={false}
+          animationConfig={config.gentle}
+        />
       </div>
       <DownButton downButton={downButton} />
     </section>
