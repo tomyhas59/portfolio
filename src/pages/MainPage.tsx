d89860c922f3a81e915b61a4ae192c21a5c448ed
이용현 <yh9035926@naver.com>
2025-07-03 17:28:32 +0900

    {gsap} -> gsap

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { gsap } from "gsap";
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import AboutMe from "../components/AboutMe";
